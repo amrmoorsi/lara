8cfd82fbdee3cbcb690e419858391b1aa0e23d8f
amrmoorsi <60606061+amrmoorsi@users.noreply.github.com>
2022-07-28 01:30:41 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/testparam/{id?}',function (){
+    return "welcome testparam";
+});
+
+
+//Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+//Route::group(['prefix'=>'/beforeRoute','namespace'=>'beforeController']);
+
+Route::resource('car','App\Http\Controllers\carController');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
